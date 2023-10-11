464f9ee09a7ff954f5917a40862378ac82f7e019
Davidoar15 <davidoar15@gmail.com>
2023-10-11 14:45:53 -0300

    update/App.java_functional_calculator

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        float Num1 = 0;
+        float Num2 = 0;
+        int option = 0;
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Write first Number:");
+            Num1 = sc.nextFloat();
+            System.out.println("Write second Number:");
+            Num2 = sc.nextFloat();
+
+            System.out.println("Write the Operation Number to Do: 1: Addition, 2: Subtraction, 3: Multiplication, 4: Division");
+            option = sc.nextInt();
+        }
+
+        operations operation = new operations(Num1, Num2);
+        float result = 0;
+
+        switch(option) {
+            case 1: result = operation.add(); break;
+            case 2: result = operation.subtract(); break;
+            case 3: result = operation.multp(); break;
+            case 4: result = operation.split(); break;
+        }
+
+        System.out.println("Result: "+result);
     }
 }
